57f162cb1b33751f935d1defbb58ef0dc3b8fe19
florinelamuscalu <mflorinelagarbiela@gmail.com>
2023-01-11 10:40:07 +0200

    rol.model api

--- ClientApp/src/app/models/api-models/user.model.ts
+++ ClientApp/src/app/models/api-models/user.model.ts
@@ -2,13 +2,12 @@ import { Rol } from "./rol.model";
 
 export interface User {
   id: string,
-  reload: string,
+  relo: string,
   email: string,
   password: string,
   firstName: string,
   lastName: string,
   address: string,
   phoneNumber: string,
-  roles: string;
   rol: Rol,
 }
