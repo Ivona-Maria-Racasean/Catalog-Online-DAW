471d7be5ff65362a786aed2e6cc4c529e8046059
Ivi123 <ivona_racasean9@yahoo.com>
2023-01-24 18:55:58 +0200

    home page

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CurrentUser } from 'app/models/ui-models/currentUser.model';
 import { NavMenuComponent } from 'app/nav-menu/nav-menu.component';
+import { AuthenticationService } from '../shared/services/authentication.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
 
-  @Input() currentUser!: CurrentUser;
-  constructor(){}
+  currentUser!: CurrentUser;
+  constructor(private AuthService: AuthenticationService) { }
+  async ngOnInit() {
+    this.currentUser = await this.AuthService.getCurrentUser().toPromise();
+    }
+
+
 }
